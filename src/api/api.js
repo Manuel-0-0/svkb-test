84a3046a7319c365e360d468f389bd5ccef9caf2
kayode ojedele <ojedele46@gmail.com>
2022-10-26 01:15:31 +0100

    working!

--- src/api/api.js
+++ src/api/api.js
@@ -2,18 +2,19 @@ import Axios from 'axios'
 import Cookies from 'js-cookie'
 
 const axiosInstance = Axios.create({
-    baseURL: 'https://sv-kb.herokuapp.com/api/v1/',
+    baseURL: 'http://localhost:8080/api/v1/',
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-    }
+    },data:{}
 })
 
-axiosInstance.interceptors.request.use((config) => {
-    if (Cookies.get('sv_token')) {
-        config.headers.Authorization = 'Bearer ' + Cookies.get('sv_token');
-    }
-    return config
-})
+// axiosInstance.interceptors.request.use((config) => {
+//     if (Cookies.get('sv_token')) {
+//         config.headers.Authorization = 'Bearer ' + Cookies.get('sv_token');
+//     }
+//     console.log(config)
+//     return config
+// })
 
 export default axiosInstance
